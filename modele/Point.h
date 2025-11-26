eaf2a0b65da0466641fd935804902e266e2c5d00
Alexis Daoust <aledaoust93@gmail.com>
2025-11-26 15:23:59 -0500

    Complete point.h

---
+++ modele/Point.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class Point {
+    public:
+        int id;
+        int x;
+        int y;
+    Point(int id_, int x_, int y_) : id(id_), x(x_), y(y_) {}
+};
