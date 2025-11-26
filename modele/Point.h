e85a6d86a729bc20ba9d52987e0c46db252d5a39
Alexis Daoust <aledaoust93@gmail.com>
2025-11-26 15:39:03 -0500

    Update class point

--- modele/Point.h
+++ modele/Point.h
@@ -3,9 +3,19 @@
 #include <vector>
 
 class Point {
-    public:
-        int id;
-        int x;
-        int y;
+public:
     Point(int id_, int x_, int y_) : id(id_), x(x_), y(y_) {}
+    int getId() const { return id; }
+    int getY() const { return y; }
+    int getX() const { return x; }
+
+    void setPosition(int newX, int newY){
+        x = newX;
+        y = newY;
+    }
+    
+private:
+    int id;
+    int x;
+    int y;
 };
