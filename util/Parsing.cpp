f82028fea42f948be1b4cd06a4965dae342dd1fa
Alexis Daoust <aledaoust93@gmail.com>
2025-11-27 18:16:37 -0500

    Complete parsing tool

---
+++ util/Parsing.cpp
@@ -0,0 +1,31 @@
+#include "util/Parsing.h"
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<Point> creerPointsDepuisLigne(const string& ligne) {
+    vector<Point> points;
+    // On crée un flux de lecture (istringstream) à partir de la chaîne ligne.
+    istringstream iss(ligne);
+    string token;
+    int id = 0;
+    // On lit chaque point de la ligne (séparé par des espaces).
+    while (iss >> token) {
+        // On vérifie que le point est entre parenthèses
+        if (token.size() >= 3 && token.front() == '(' && token.back() == ')') {
+            // On enlève les parenthèses au début et à la fin.
+            token = token.substr(1, token.size() - 2);
+            // On remplace la virgule par un espace.
+            replace(token.begin(), token.end(), ',', ' ');
+            // On crée un flux de lecture pour chaque point
+            istringstream pair(token);
+            int x, y;
+            // On ajoute un point {x, y} au vecteur de points.
+            if (pair >> x >> y) {
+                points.emplace_back(id++, x, y);
+            }
+        }
+    }
+    return points;
+}
---
+++ util/Parsing.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "modele/Point.h"
+
+std::vector<Point> creerPointsDepuisLigne(const std::string& ligne);
